f6689379414eed77606f70a154448e2b8855e056
CS107 tools <cs107@cs.stanford.edu>
2023-03-12 17:10:17 -0700

    successful make.

--- implicit.c
+++ implicit.c
@@ -1,6 +1,37 @@
 #include "./allocator.h"
 #include "./debug_break.h"
 
+#define HEADER_SIZE 8
+static void *segment_start;
+static size_t segment_size;
+
+typedef struct {
+    size_t size;
+} header;
+
+size_t roundup(size_t sz, size_t mult) {
+    return (sz + mult - 1) & ~(mult - 1);
+}
+
+bool is_free(void *headerptr) {
+    header *header_ptr = (header *)headerptr;
+    if ((header_ptr->size & 1) == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void make_used(void *headerptr, size_t requested_size) {
+    header *header_ptr = (header *)headerptr;
+    header_ptr->size = requested_size + 1;
+}
+
+void make_free(void *ptr, size_t space) {
+    header *header_ptr = (header *)ptr;
+    header_ptr->size = space;
+}
+    
 bool myinit(void *heap_start, size_t heap_size) {
     /* TODO(you!): remove the line below and implement this!
      * This must be called by a client before making any allocation
@@ -10,13 +41,61 @@ bool myinit(void *heap_start, size_t heap_size) {
      * against a set of of test scripts, our test harness calls
      * myinit before starting each new script.
      */
-    return false;
+    if (heap_size <= HEADER_SIZE) {
+        return false;
+    }
+    segment_start = heap_start;
+    segment_size = heap_size;
+    header *first_header = (header *)heap_start;
+    first_header->size = heap_size - HEADER_SIZE;
+    return true;
 }
 
 void *mymalloc(size_t requested_size) {
-    // TODO(you!): remove the line below and implement this!
-    return NULL;
+    void *result = NULL;
+    if (requested_size == 0) {
+        return result;
+    }
+    size_t needed = roundup(requested_size, ALIGNMENT);
+    void *temp = segment_start;
+    void *end_heap =  (char *)segment_start + segment_size;
+    while (temp < end_heap) {
+        if (is_free(temp)) {
+            if (needed <= ((header *)temp)->size) {
+                long space = ((header *)temp)->size - (needed + HEADER_SIZE);
+                result = (char *)temp + HEADER_SIZE;
+                if (space == 0) {
+                    make_used(temp, needed + HEADER_SIZE);
+                    return result;
+                } else if (space >= 0) {
+                    make_used(temp, needed);
+                    temp = (char *)temp + HEADER_SIZE + needed;
+                    make_free(temp, space);
+                    return result;
+                } else {
+                    make_used(temp, needed);
+                    return result;
+                }
+            } else {
+                temp = (char *)temp + HEADER_SIZE + ((header *)temp)->size;
+            }
+        } else {
+            temp = (char *)temp + HEADER_SIZE + (((header *)temp)->size - 1);
+        }
+    }
+    return result;
 }
+                
+                
+                
+                
+                
+                
+                
+                
+            
+        
+
 
 void myfree(void *ptr) {
     // TODO(you!): implement this!
@@ -36,7 +115,8 @@ bool validate_heap() {
      * You can also use the breakpoint() function to stop
      * in the debugger - e.g. if (something_is_wrong) breakpoint();
      */
-    return false;
+    
+    return true;
 }
 
 /* Function: dump_heap
